4c78b9c99b326d549332e18a854069652039d632
henryalps <henryalps@gmail.com>
2015-12-04 23:20:21 +0800

    修改判断信号无效所用阈值

--- BIsSignalLegal.m
+++ BIsSignalLegal.m
@@ -1,7 +1,7 @@
 function isLegal = BIsSignalLegal(sig)
-    THEROLD = 0.3;
+    THEROLD = 0.4;
     %% 先把所有的NaN替换掉
-    
+    sig = BRemoveNan(sig);
     %% 求一阶差分零值数
     d1 = (len(diff(sig)~=0)) ;
     %% 若一阶差分的非零率小于阈值，说明大部分数据为无效数据
@@ -9,4 +9,13 @@ function isLegal = BIsSignalLegal(sig)
         isLegal = FALSE;
         return
     end
+end
+
+function isLegal = BIsBPPosLegal(bppos)
+    config = BReadConfig();
+    isLegal = length(bppos) > config.timelength;
+end
+
+function isLegal = BIsEcgPosLegal(rpos)
+    isLegal = BIsBPPosLegal(rpos);
 end
