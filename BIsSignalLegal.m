e8043de124ce5ebdf1f20d5ded872a40f586474b
henryalps <henryalps@gmail.com>
2015-12-04 23:35:53 +0800

    粗略地进行信号筛选

--- BIsSignalLegal.m
+++ BIsSignalLegal.m
@@ -7,13 +7,14 @@ end
 
 function isLegal = BIsSignalMostlyEffective(sig)
     THEROLD = 0.4;
+    isLegal = true;
     %% 先把所有的NaN替换掉
     sig = BRemoveNan(sig);
     %% 求一阶差分零值数
     d1 = (len(diff(sig)~=0)) ;
     %% 若一阶差分的非零率小于阈值，说明大部分数据为无效数据
     if d1/len(sig) < THEROLD
-        isLegal = FALSE;
+        isLegal = false;
         return
     end
 end
