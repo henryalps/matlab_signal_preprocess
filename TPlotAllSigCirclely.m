315c57417c785f5005a3f56dda9c75d115f4a60d
henryalps <henryalps@gmail.com>
2015-12-06 11:27:51 +0800

    Corrify last commit

--- TPlotAllSigCirclely.m
+++ TPlotAllSigCirclely.m
@@ -1,21 +1,12 @@
-function TPlotAllSigCirclely()
-    POOL = parpool('local',8);
-    load('newNewMatNames.mat');
-    newNewMatNames = {};
-    matNamesSelectedByDistribute1 = ones(1,length(newNewMatNames));
-    parfor i = 1:length(newNewMatNames)
+function matNamesSelectedByDistribute1=TPlotAllSigCirclely()
+    newNewMatNames=load('newNewMatNames.mat');
+    newNewMatNames= newNewMatNames.newNewMatNames;
+    matNamesSelectedByDistribute1 = ones(1,length(newNewMatNames))*true;
+    for i = 1:length(newNewMatNames)
         tmp = load(newNewMatNames{i});
-        tmp = tmp.newNewMatNames;
-%         TPlogAllSig(bp,ecg,ppg,bpann,rpos,tm);
-%         disp(newNewMatNames{i})
-        if ~BSimplelySelectSigByDistribute(tmp.bp,tmp.ecg,tmp.ppg,...
-                tmp.bpann,tmp.rpos,tmp.tm)
-%             disp('illegal');
-            matNamesSelectedByDistribute1(i)=0;
-        end
-%         pause
+        TPlogAllSig(tmp.bp,tmp.ecg,tmp.ppg,tmp.bpann,tmp.rpos,tmp.tm);
+        disp(newNewMatNames{i})
+        pause
     end
-    matNamesSelectedByDistribute1 = newNewMatNames{logical(matNamesSelectedByDistribute1)};
-    save('matNamesSelectedByDistribute1.mat','matNamesSelectedByDistribute1');
-    delete(POOL)
+  
 end
